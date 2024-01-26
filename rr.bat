6fa5aa14f6902fdc90b613477027a4c46d8cc7fe
DerKEKomat3000 <107874898+DerKEKomat3000@users.noreply.github.com>
2024-01-26 09:00:17 +0100

    Update rr.bat

--- rr.bat
+++ rr.bat
@@ -1,10 +1,10 @@
 @echo off
 set UserDownloadsPath=%USERPROFILE%\Downloads
-set ImageURL=https://example.com/your_image_url.jpg
-set BGImagePath=%UserDownloadsPath%\test.png
+set GitHubRepoURL=https://raw.githubusercontent.com/DerKEKomat3000/test/de6a8427f6f13ea2fe94db70e09ae7c4409b2c2c/rick.png
+set BGImagePath=%UserDownloadsPath%\rick.png
 
 rem Download the image using curl
-curl -o "%BGImagePath%" "%ImageURL%"
+curl -o "%BGImagePath%" "%GitHubRepoURL%"
 
 rem Set the desktop background
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%BGImagePath%" /f
