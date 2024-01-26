bb092cd9477c21d9562cc9a5e1e01bc6962914cb
DerKEKomat3000 <107874898+DerKEKomat3000@users.noreply.github.com>
2024-01-26 08:50:05 +0100

    Update rr.bat

--- rr.bat
+++ rr.bat
@@ -1,3 +1,6 @@
 @echo off
-reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "C:\Path\To\Your\Image.jpg" /f
+set UserDownloadsPath=%USERPROFILE%\Downloads
+set BGImagePath=%UserDownloadsPath%\YourImage.jpg
+
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%BGImagePath%" /f
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
