ce1d192627831c295aab525974264b6157136630
DerKEKomat3000 <107874898+DerKEKomat3000@users.noreply.github.com>
2024-01-26 14:57:00 +0100

    Update video.bat

--- video.bat
+++ video.bat
@@ -6,5 +6,10 @@ rem sndvol.exe /changevolume -100
 rem Increase volume by 10
 sndvol.exe /changevolume +50
 
+set UserDownloadsPath=%USERPROFILE%\Downloads
+set GitHubRepoURL=https://raw.githubusercontent.com/DerKEKomat3000/test/main/RR.mp4
+set BGImagePath=%UserDownloadsPath%\RR.mp4
 
-curl -L -O https://raw.githubusercontent.com/DerKEKomat3000/test/main/RR.mp4
+rem Download the image using curl
+curl -o "%BGImagePath%" "%GitHubRepoURL%"
+
