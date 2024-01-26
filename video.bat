8cfa594585451520d7371e829d6a321691c1b569
DerKEKomat3000 <107874898+DerKEKomat3000@users.noreply.github.com>
2024-01-26 15:14:15 +0100

    Update video.bat

--- video.bat
+++ video.bat
@@ -1,11 +1,5 @@
 @echo off
 
-rem Decrease volume by 10
-sndvol.exe /changevolume -100
-
-rem Increase volume by 10
-sndvol.exe /changevolume +50
-
 set UserDownloadsPath=%USERPROFILE%\Downloads
 set GitHubRepoURL=https://raw.githubusercontent.com/DerKEKomat3000/test/main/RR.mp4
 set BGImagePath=%UserDownloadsPath%\RR.mp4
