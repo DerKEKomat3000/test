d7f9afe84bd7b3bcf3c0f1882586db55777024e0
DerKEKomat3000 <107874898+DerKEKomat3000@users.noreply.github.com>
2024-01-26 08:52:31 +0100

    Update rr.bat

--- rr.bat
+++ rr.bat
@@ -1,6 +1,11 @@
 @echo off
 set UserDownloadsPath=%USERPROFILE%\Downloads
-set BGImagePath=%UserDownloadsPath%\YourImage.jpg
+set ImageURL=https://example.com/your_image_url.jpg
+set BGImagePath=%UserDownloadsPath%\test.png
 
+rem Download the image using curl
+curl -o "%BGImagePath%" "%ImageURL%"
+
+rem Set the desktop background
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%BGImagePath%" /f
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
