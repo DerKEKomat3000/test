0c7a28906a6246932bc9576a6dc956be419c9d00
DerKEKomat3000 <107874898+DerKEKomat3000@users.noreply.github.com>
2024-01-27 12:41:53 +0100

    Update video.bat

--- video.bat
+++ video.bat
@@ -4,7 +4,13 @@ set UserDownloadsPath=%USERPROFILE%\Downloads
 set GitHubRepoURL=https://raw.githubusercontent.com/DerKEKomat3000/test/main/RR.mp4
 set BGImagePath=%UserDownloadsPath%\RR.mp4
 
+if exist "%BGImagePath%" (
+
+    goto :VIEW_VIDEO
+)
+
 rem Download the image using curl
 curl -o "%BGImagePath%" "%GitHubRepoURL%"
 
+:VIEW_VIDEO
 start "Movies & TV" "%USERPROFILE%\Downloads\RR.mp4"
